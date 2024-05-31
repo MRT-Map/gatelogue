135ac753f106f7433d6cc6f360fae3171806a238
7d <61975820+iiiii7d@users.noreply.github.com>
2024-05-31 19:56:42 +0800

    fix eslint

---
+++ gatelogue-client/eslint.config.js
@@ -0,0 +1,32 @@
+// @ts-check
+
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginVue from "eslint-plugin-vue";
+import vueParser from "vue-eslint-parser";
+import prettierConfig from "eslint-config-prettier";
+
+
+export default tseslint.config(
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  //@ts-ignore
+  ...pluginVue.configs["flat/essential"],
+  {
+    plugins: {
+      'typescript-eslint': tseslint.plugin,
+    },
+    languageOptions: {
+      globals: globals.browser,
+      parser: vueParser,
+      parserOptions: {
+      parser: tseslint.parser,
+      //project: "./tsconfig.app.json",
+      //extraFileExtensions: ['.vue'],
+      sourceType: 'module'
+      }
+    },
+  },
+  prettierConfig
+);
