dde2ea3723b0396fc9ac43bea7dc2570b04a8c29
7d <61975820+iiiii7d@users.noreply.github.com>
2024-06-02 13:51:11 +0800

    update @ 20240602-13:51:11+08

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-pipx install --force --python python3.12 git+https://github.com/mrt-map/gatelogue#subdirectory=gatelogue-aggregator
-~/.local/share/pipx/venvs/gatelogue-aggregator/bin/gatelogue-aggregator run -o data.json
-~/.local/share/pipx/venvs/gatelogue-aggregator/bin/gatelogue-aggregator schema -o schema.json
+pipx reinstall gatelogue-aggregator || pipx install --python python3.12 git+https://github.com/mrt-map/gatelogue#subdirectory=gatelogue-aggregator
+gatelogue-aggregator run -o data.json
+gatelogue-aggregator schema -o schema.json
 git commit -am "update @ $(date +%Y%m%d-%H:%M:%S%Z)"
