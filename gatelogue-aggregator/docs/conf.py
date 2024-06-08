a134fb43c32358bde71c86e7bec840877384668d
7d <61975820+iiiii7d@users.noreply.github.com>
2024-06-08 22:33:15 +0800

    add more mappings

--- gatelogue-aggregator/docs/conf.py
+++ gatelogue-aggregator/docs/conf.py
@@ -24,7 +24,11 @@ extensions = [
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
-intersphinx_mapping = {"python": ("https://docs.python.org/3", None)}
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+    "bs4": ("https://www.crummy.com/software/BeautifulSoup/bs4/doc/", None),
+    "networkx": ("https://networkx.org/documentation/stable/", None),
+}
 html_baseurl = "https://mrt-map.github.io/gatelogue/docs"
 
 # -- Options for HTML output -------------------------------------------------
