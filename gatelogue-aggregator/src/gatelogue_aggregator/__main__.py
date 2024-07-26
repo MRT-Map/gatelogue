ba93b9ca5e25c74ec8ce471566568ad9a9702a41
iiiii7d <iiiii7d@users.noreply.github.com>
2024-07-26 11:38:00 +0000

    deploy: bce73cbfd0383dcf366468177cc2665d1618836d

--- gatelogue-aggregator/src/gatelogue_aggregator/__main__.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-if __name__ == "__main__":
-    from gatelogue_aggregator.cli import gatelogue_aggregator
-
-    sys.exit(gatelogue_aggregator())
--- gatelogue-client/env.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="vite/client" />
-// eslint-disable-next-line init-declarations
-declare const APP_VERSION: string;
