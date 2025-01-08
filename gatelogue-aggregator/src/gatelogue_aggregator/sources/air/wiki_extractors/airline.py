6550084cea71107fc030e019a8dbe92877c806ec
7d <61975820+iiiii7d@users.noreply.github.com>
2025-01-08 11:19:53 +0800

    fix intraair

--- gatelogue-aggregator/src/gatelogue_aggregator/sources/air/wiki_extractors/airline.py
+++ gatelogue-aggregator/src/gatelogue_aggregator/sources/air/wiki_extractors/airline.py
@@ -67,6 +67,8 @@ def intra_air(ctx: WikiAirline, config):
     result = 0
     for table in html("table"):
         for tr in table("tr")[1::4]:
+            if len(tr("td")) < 7:
+                continue
             if tr("td")[6].find("a", href="/index.php/File:Rsz_open.png") is None:
                 continue
             code = tr("td")[1].span.string.removeprefix("Flight ")
