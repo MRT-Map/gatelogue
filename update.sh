9ab99426342bfc33cfa52e3e1687ad49e283903e
7d <61975820+iiiii7d@users.noreply.github.com>
2025-04-13 18:58:36 +0800

    manual update

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-pipx reinstall gatelogue-aggregator || pipx install --python python3.12 --system-site-packages git+https://github.com/mrt-map/gatelogue#subdirectory=gatelogue-aggregator
+pipx reinstall gatelogue-aggregator || pipx install --system-site-packages git+https://github.com/mrt-map/gatelogue#subdirectory=gatelogue-aggregator
 gatelogue-aggregator run -o data.json -g graph.svg "$@"
 gatelogue-aggregator schema -o schema.json
-python3.12 remove_sources.py
+python remove_sources.py
