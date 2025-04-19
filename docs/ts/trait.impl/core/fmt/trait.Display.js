c5498b268b5da9fb3a21e79dc1c44f0970f5d513
iiiii7d <iiiii7d@users.noreply.github.com>
2025-04-19 04:48:06 +0000

    deploy: e196a4f1019b59926863fa478c1b620a66fd4a74

---
+++ docs/ts/trait.impl/core/fmt/trait.Display.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["gatelogue_types",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"gatelogue_types/enum.Error.html\" title=\"enum gatelogue_types::Error\">Error</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[277]}
