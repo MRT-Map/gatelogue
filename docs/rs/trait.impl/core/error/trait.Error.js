a0f9c571aca0fec1eb3222ee5da61604cb62501a
iiiii7d <iiiii7d@users.noreply.github.com>
2025-05-19 02:57:54 +0000

    deploy: c3225c894630ce4c69517cc4646370f47584e597

--- docs/rs/trait.impl/core/error/trait.Error.js
+++ docs/rs/trait.impl/core/error/trait.Error.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["gatelogue_types",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"gatelogue_types/enum.Error.html\" title=\"enum gatelogue_types::Error\">Error</a>"]]]]);
+    var implementors = Object.fromEntries([["gatelogue_types",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"gatelogue_types/enum.Error.html\" title=\"enum gatelogue_types::Error\">Error</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
