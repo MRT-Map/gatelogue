c047526afba29ecd2cd2431f05adf65059b13420
iiiii7d <iiiii7d@users.noreply.github.com>
2025-12-11 22:47:46 +0000

    deploy: 638575f67ac02889d27c3b302defe9f7d9431639

--- docs/rs/trait.impl/core/convert/trait.From.js
+++ docs/rs/trait.impl/core/convert/trait.From.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["gatelogue_types",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.1/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"struct\" href=\"https://docs.rs/serde_json/1.0.145/serde_json/error/struct.Error.html\" title=\"struct serde_json::error::Error\">Error</a>&gt; for <a class=\"enum\" href=\"gatelogue_types/enum.Error.html\" title=\"enum gatelogue_types::Error\">Error</a>"]]]]);
+    var implementors = Object.fromEntries([["gatelogue_types",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.92.0/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"struct\" href=\"https://docs.rs/serde_json/1.0.145/serde_json/error/struct.Error.html\" title=\"struct serde_json::error::Error\">Error</a>&gt; for <a class=\"enum\" href=\"gatelogue_types/enum.Error.html\" title=\"enum gatelogue_types::Error\">Error</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
