f94f30a3f3aecb91e46ebd4e9bfd690532d86d8f
7d <61975820+iiiii7d@users.noreply.github.com>
2026-01-04 15:55:12 +0800

    fix erzlink trams

--- gatelogue-aggregator/src/gatelogue_aggregator/sources/rail/local/erzlink_trams.py
+++ gatelogue-aggregator/src/gatelogue_aggregator/sources/rail/local/erzlink_trams.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 from gatelogue_aggregator.sources.yaml2source import Yaml2Source, YamlLine
 from gatelogue_aggregator.types.node.rail import RailCompany, RailLine, RailLineBuilder, RailSource, RailStation
+from gatelogue_aggregator.utils import get_stn
 
 
 class ErzLinkTrams(Yaml2Source, RailSource):
@@ -20,6 +21,27 @@ class ErzLinkTrams(Yaml2Source, RailSource):
         if line_node.code == "0":
             self.B(self, line_node).circle(
                 *stations, forward_label=line_yaml.forward_label, backward_label=line_yaml.backward_label
+            )
+        elif line_node.code == "3":
+            self.B(self, line_node).connect(
+                *stations, between=(None, "Atrium North"), forward_label=line_yaml.forward_label, backward_label=line_yaml.backward_label
+            )
+            self.B(self, line_node).connect(
+                *stations, between=("Atrium South", None), forward_label=line_yaml.forward_label, backward_label=line_yaml.backward_label
+            )
+            self.B(self, line_node).connect(
+                get_stn(stations, "Atrium North"),
+                get_stn(stations, "Atrium East"),
+                get_stn(stations, "Atrium South"),
+                one_way=True,
+                forward_label=line_yaml.forward_label, backward_label=line_yaml.backward_label
+            )
+            self.B(self, line_node).connect(
+                get_stn(stations, "Atrium South"),
+                get_stn(stations, "Atrium West"),
+                get_stn(stations, "Atrium North"),
+                one_way=True,
+                forward_label=line_yaml.backward_label, backward_label=line_yaml.forward_label
             )
         else:
             raise NotImplementedError
