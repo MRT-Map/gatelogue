bdeb9929f9d489fc12ec997ddf9c2bf63bf478f9
7d <61975820+iiiii7d@users.noreply.github.com>
2026-02-26 00:09:49 +0800

    setup dist for v3

--- update.sh
+++ update.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-pipx reinstall gatelogue-aggregator || pipx install --system-site-packages git+https://github.com/mrt-map/gatelogue#subdirectory=gatelogue-aggregator
-gatelogue-aggregator run -o data.json -g graph.svg "$@"
-gatelogue-aggregator schema -o schema.json
-python remove_sources.py
+pipx reinstall gatelogue-aggregator || pipx install --system-site-packages git+https://github.com/mrt-map/gatelogue@v3#subdirectory=gatelogue-aggregator
+gatelogue-aggregator run "$@"
+gatelogue-aggregator drop-sources
