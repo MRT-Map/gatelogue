5813af7d93bda99a50ab012d5fe4590ebaf2729a
7d <61975820+iiiii7d@users.noreply.github.com>
2026-02-27 23:04:42 +0800

    remove all dbs before running (not sure if it changes anything)

--- update.sh
+++ update.sh
@@ -2,5 +2,6 @@
 set -euxo pipefail
 
 uv tool install -Up 3.14 git+https://github.com/mrt-map/gatelogue@v3#subdirectory=gatelogue-aggregator
+rm ./data.db ./data-ns.db
 gatelogue-aggregator run "$@"
 gatelogue-aggregator drop-sources
