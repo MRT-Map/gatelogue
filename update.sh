e2e26886f9c60b371f1ecc2f7561cf032e1c9f6c
7d <61975820+iiiii7d@users.noreply.github.com>
2026-02-28 17:00:19 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,5 +3,5 @@ set -euxo pipefail
 
 uv tool install -Up 3.14 git+https://github.com/mrt-map/gatelogue@v3#subdirectory=gatelogue-aggregator
 rm ./data.db ./data-ns.db
-gatelogue-aggregator run "$@"
+gatelogue-aggregator run "$@" | tee log.log
 gatelogue-aggregator drop-sources
