06c092bf5c9d5cc833433dcb05e1bac4a83d5dce
7d <61975820+iiiii7d@users.noreply.github.com>
2026-03-10 09:56:25 +0800

    index db

--- gatelogue-client/src/stores/data.ts
+++ gatelogue-client/src/stores/data.ts
@@ -4,5 +4,15 @@ import { GD } from "gatelogue-types";
 export const gd: Ref<GD | null> = ref(null);
 
 GD.get().then((res) => {
+  res.db.run(`
+    CREATE INDEX AirAirlineNameIndex ON AirAirline(name);
+    CREATE INDEX AirFlightAirlineIndex ON AirFlight(airline);
+    CREATE INDEX AirFlightFromIndex ON AirFlight("from");
+    CREATE INDEX AirFlightToIndex ON AirFlight("to");
+    CREATE INDEX AirGateAirlineIndex ON AirGate(airline);
+    CREATE INDEX AirGateAirportIndex ON AirGate(airport);
+    CREATE INDEX AirAirportCodeIndex ON AirAirport(code);
+    CREATE INDEX NodeTypeIndex ON Node(type);
+  `);
   gd.value = res;
 });
