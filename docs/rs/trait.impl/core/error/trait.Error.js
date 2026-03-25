46dc5e547eddc91f6c26fea05e3353fdbcfa3ac1
iiiii7d <iiiii7d@users.noreply.github.com>
2026-03-25 08:22:27 +0000

    deploy: cc0a805cc4d57c2a8f1621485f815de074d6dbbd

--- docs/rs/trait.impl/core/error/trait.Error.js
+++ docs/rs/trait.impl/core/error/trait.Error.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["gatelogue_types",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"gatelogue_types/enum.Error.html\" title=\"enum gatelogue_types::Error\">Error</a>",0]]]]);
+    const implementors = Object.fromEntries([["gatelogue_types",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"gatelogue_types/enum.Error.html\" title=\"enum gatelogue_types::Error\">Error</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[277]}
+//{"start":59,"fragment_lengths":[278]}
--- docs/rs/trait.impl/core/fmt/trait.Display.js
+++ docs/rs/trait.impl/core/fmt/trait.Display.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["gatelogue_types",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"gatelogue_types/enum.Error.html\" title=\"enum gatelogue_types::Error\">Error</a>",0]]]]);
+    const implementors = Object.fromEntries([["gatelogue_types",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"gatelogue_types/enum.Error.html\" title=\"enum gatelogue_types::Error\">Error</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[279]}
+//{"start":59,"fragment_lengths":[280]}
