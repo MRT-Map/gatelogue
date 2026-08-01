915e2162048da3660ecd3b56f19d9030569af20c
7d <61975820+iiiii7d@users.noreply.github.com>
2026-08-01 13:37:02 +0800

    also dump sql

--- update.sh
+++ update.sh
@@ -5,3 +5,5 @@ uv tool install -Up 3.14 git+https://github.com/mrt-map/gatelogue#subdirectory=g
 rm ./data.db ./data-ns.db
 gatelogue-aggregator run "$@" | tee log.log
 gatelogue-aggregator drop-sources
+sqlite3 data.db .dump > data.sql
+sqlite3 data-ns.db .dump > data-ns.sql
